360e2c9ea6b3e2946e0e8a5e105eb674cd899ceb
Sonnax <nate@theklaibers.com>
2010-11-10 16:50:57 +0000

    adding files to the PHP app

--- sonnax-html/solenoids.php
+++ sonnax-html/solenoids.php
@@ -131,23 +131,21 @@ font-size:100%;
 
   <table width="783" border="0" align="center">
     <tr>
-      <td width="413" height="199" align="left" valign="bottom"><span class="FirstHeader">Solenoid Core Purchase Program*</span>
-<p class="subhead">We make it FAST and EASY to return your Solenoid cores!</p>
+      <td width="413" align="left" valign="top">
+      <span class="FirstHeader">Solenoid Core Purchase Program*</span>
+		<p class="subhead">We make it FAST and EASY to return your solenoid cores!</p>
        <ol> <ol class="content">
           <li><span class="subhead">Collect Cores </span>Five or more cores is recommended.</li>
-          <li><span class="subhead">Contact Us </span>We’ll set up a Return Authorization for you.</li>
+          <li><span class="subhead">Contact Us </span>We'll set up a Return Authorization for you.</li>
           <li><span class="subhead">Ship Your Cores</span> We pre-pay all shipping charges and provide labels.</li>
-          <li><span class="subhead">GET PAID!</span> We’ll send you a check in the mail.</li>
+          <li><span class="subhead">GET PAID!</span> We'll send you a check in the mail.</li>
         </ol></ol>
-        <p class="sonnaxinfo2">*Continental U.S. only. If you are a customer outside the continental U.S. with many Solenoid cores, please contact us at (802) 463-0379 to discuss your return.</p>
-      </td>
-      <td width="360" valign="top"><span class="subhead">We buy these Solenoid cores</span>
-<img src="solenoids-4.gif" width="254" height="215" alt="Solenoids we buy"></td>
- 
-    <tr>
+        <p class="sonnaxinfo2">*Continental U.S. only. If you are a customer outside the continental U.S. with many solenoid cores, please contact us at (802) 463-0379 to discuss your return.</p>
+        
+        
+        
     <form name='register' id='register' method='get' action='solenoids_exe.php' class='form'>
-      <td class="content">
-      <table width="404" border="0" align="left" cellpadding="0">
+      <table border="0" width="413" cellpadding="0">
         <tr>
           <td height="21" colspan="3" valign="top" class="subhead">Start your core return:</td>
         </tr>
@@ -233,7 +231,7 @@ font-size:100%;
           <td height="31" colspan="3">
             <input type="submit" name="button" id="button" value="Submit">
             &nbsp;
-            <input type="Reset" name="button" id="button" value="Reset">
+            <input type="Reset" name="button2" id="button2" value="Reset">
         </td>
         </tr>
         
@@ -241,12 +239,18 @@ font-size:100%;
           <td height="27" valign="top"><div align="right"><span class="asterix">*</span>Required fields</div></td></tr>
         </table>
       </form>
-    
-      <td valign="top" rowspan="2">
-       <span class="subhead">Please review the following guidelines before returning Solenoid cores:</span><br>
+
+        <p class="sonnaxinfo2">We will contact you within one business day to start processing your return. You may also return solenoid cores by contacting us directly at (800) 843-2600, Ext. 379, Monday to Friday, 8:30 a.m. to 5 p.m. ET.</p>
+        
+        
+      </td>
+      <td valign="top">
+      <p class="subhead">We buy these solenoid cores</p>
+      <div align="center"><img align="middle" src="solenoids-6.jpg" alt="Solenoids we buy"></div>
+       <p class="subhead">Please review the following guidelines before returning solenoid cores:</p>
         <ul type="disc" class="content"> 
         <li>
-          <p>Saving cores until you have five or more is recommended, as this streamlines the process and expedites your return. We accept 45RFE/545RFE Early Style, 45RFE/545RFE/68RFE Late Style and 5R55N/W/S (OEM and remanufactured) Solenoids.</p>
+          <p>Saving cores until you have five or more is recommended, as this streamlines the process and expedites your return. We accept Chrysler 45RFE (early and late model) and 62TE solenoid cores as well as Ford 5R55N/W/S and 4R100 solenoid cores.</p>
         </li>
           <li>
             <p>All shipments must have a Return Authorization in order to be processed. Shipping charges are paid by Sonnax.</p>
@@ -255,10 +259,10 @@ font-size:100%;
             <p>Solenoids must be packaged in plastic bags before being placed in a box, to ensure any oil will not leak and create shipping issues with UPS.</p>
           </li>
           <li>
-            <p>Due to UPS shipping standards, please limit individual box weight to 75 lbs. This would be approximately 20 Solenoids per box.</p>
+            <p>Due to UPS shipping standards, please limit individual box weight to 75 lbs. This would be approximately 20 solenoids per box.</p>
           </li>
           <li>
-            <p>If you have a large quantity of Solenoids and require more than one box, Sonnax will issue one return label for each box. If the shipment is too large for UPS, Sonnax will arrange for a common carrier to pick up.</p>
+            <p>If you have a large quantity of solenoids and require more than one box, Sonnax will issue one return label for each box. If the shipment is too large for UPS, Sonnax will arrange for a common carrier to pick up.</p>
           </li>
           <li>
             <p>Sonnax will issue a check within 60 days of the shipment being sent from your facility.</p>
@@ -266,11 +270,8 @@ font-size:100%;
           <li>
             <p>Unusable parts or parts not meeting the criteria will not be returned or credited.</p>
           </li>
-      </ul></td>
-    </tr>
-    <tr>
-      <td valign="top" class="opening-paragraph"><p class="sonnaxinfo2">We will contact you within one business day to start processing your return. You may also return Solenoid cores by contacting us directly at (800) 843-2600, Ext. 379, Monday to Friday, 8:30 a.m. to 5 p.m. ET.</p></td>
-      
+      </ul>
+      </td>
     </tr>
   </table>
 
